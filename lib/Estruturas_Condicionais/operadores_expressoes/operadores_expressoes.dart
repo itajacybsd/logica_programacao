8417887a136ace1e324a89cfd583276f8974307f
itajacy TI02 <itajacy@bsd.com.br>
2026-03-25 16:07:13 -0300

    operadores expressoes

---
+++ lib/Estruturas_Condicionais/operadores_expressoes/operadores_expressoes.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  // Operadores
+
+  // > Maior que
+  // < Menor que
+  // >= Maior ou igual a
+  //  <= Menor ou igual a
+  // == Igual a
+  // != Diferente de
+  //  Expressão Relaciona l ==>   ( 3 < 4)
+  // ==================================
+
+  var idade = 40;
+  print(idade > 18); // true
+
+  // && E lógico
+  // || Ou lógico
+  // ! Negação lógica
+  // % Resto da divisão
+  // + Adição
+  // - Subtração
+  // * Multiplicação
+  // / Divisão
+  // ~/ Divisão inteira
+  // ++ Incremento
+  // -- Decremento
+  // = Atribuição
+  // ?? Atribuição condicional
+  // ??= Atribuição condicional composta
+}
