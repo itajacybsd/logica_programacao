d972c9d98c1465cad3a2ff634d2bccb9ef8becfc
itajacy TI02 <itajacy@bsd.com.br>
2026-04-01 16:32:57 -0300

    matriz  -  inicio da pratica3

---
+++ lib/matriz/pratica3.dart
@@ -0,0 +1,63 @@
+/* 
+    Agora para finalizar você deve permitir a compra de mais de um ingresso.
+    por usuário.
+    Regras:
+      - Validar se a fileira escolhida existe
+      - Validar se a poltrona escolhida existe
+      - Validar se a poltrona escolhida está disponível para compra.
+*/
+
+import 'dart:io';
+
+void main(List<String> args) {
+  var teatro = [];
+  var fileiras = 7;
+  var poltronasPorFileira = 10;
+
+  for (var fileira = 0; fileira < fileiras; fileira++) {
+    var fileiraTeatro = [];
+    for (var poltrona = 0; poltrona < poltronasPorFileira; poltrona++) {
+      fileiraTeatro.add(0);
+    }
+    teatro.add(fileiraTeatro);
+  }
+
+  bool querComprar = true;
+  while (querComprar) {
+    print("Deseja Comprar(S/N)");
+    String desejoComprar = stdin.readLineSync() as String;
+
+    if (desejoComprar != "N" && desejoComprar != "S") {
+      print("Opção inválida, digite S ou N");
+    } else if (desejoComprar == "N") {
+      querComprar = false;
+    } else {
+      print("Digite o número da fileira desejada (0-${fileiras - 1})");
+      int fileiraEscolhida = int.parse(stdin.readLineSync() as String);
+
+      print(
+        "Digite o número da poltrona desejada (0-${poltronasPorFileira - 1})",
+      );
+      int poltronaEscolhida = int.parse(stdin.readLineSync() as String);
+
+      print("        Mapa do Teatro");
+      print("------------------------------");
+      print("------------palco------------\n");
+
+      for (var fileira = 0; fileira < fileiras; fileira++) {
+        var fileiraP = '';
+        for (var poltrona = 0; poltrona < poltronasPorFileira; poltrona++) {
+          if ((fileira == fileiraEscolhida) &&
+              (poltrona == poltronaEscolhida)) {
+            fileiraP += '******** | ';
+          } else {
+            fileiraP += 'P($fileira$poltrona): ${teatro[fileira][poltrona]} | ';
+          }
+        }
+        print(fileiraP);
+      }
+      print("\n");
+      print("--------final do teatro-------\n");
+    }
+  }
+}
