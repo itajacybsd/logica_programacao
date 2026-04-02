d90b73823e396360c3d81a819f78adf9e4039f60
itajacy TI02 <itajacy@bsd.com.br>
2026-04-02 16:46:44 -0300

    referencia de memoria

---
+++ lib/funcao/parametros/referencia.dart
@@ -0,0 +1,9 @@
+// Tipos Primitivos (int, double, bool, String)
+//  É ENVIADA UMA CÓPIA DA VARIÁVEL EM MEMÓRIA
+
+
+// Objetos e Coleções (listas, mapas, instâncias de classes)
+// É ENVIADA A POSIÇÃODA VARIÁVEL NA MEMÓRIA(PONTEIRO)
+
+
+
